aa79912d64fa517ddb877718fbfa88492f20b1ef
sgp28 <simonprovstdev@gmail.con>
2023-03-27 08:06:45 +0100

    feat(templates): add custom base estimator
    Custom base estimator is a base estimator from sklearn that simply performs a Check X Y on the data passed as input to verify that the data is correctly shaped for any fit, fit transform.

---
+++ scikit_longitudinal/templates/__init__.py
@@ -0,0 +1,3 @@
+from .custom_base_estimator import CustomBaseEstimator
+
+__all__ = ["CustomBaseEstimator"]
---
+++ scikit_longitudinal/templates/custom_base_estimator.py
@@ -0,0 +1,56 @@
+from functools import wraps
+from typing import Any, Callable
+
+import numpy as np
+from overrides import EnforceOverrides, final
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_array, check_X_y
+
+
+class CustomBaseEstimator(BaseEstimator, TransformerMixin, EnforceOverrides):
+    """
+    A custom base class for scikit-learn estimators that automatically checks input data
+    using the check_X_y and check_array functions from sklearn.utils.validation.
+
+    Subclasses should implement the _fit and _transform methods.
+
+    Methods:
+        cannot be overriden - fit(X, y=None): Calls the _fit method, automatically checking X and y before calling.
+        cannot be overriden - transform(X): Calls the _transform method, automatically checking X before calling.
+        needs to be overriden - _fit(X, y=None): To be implemented by subclasses, contains the actual fitting logic.
+        needs to be overriden -  _transform(X): To be implemented by subclasses, contains the actual transforming logic.
+    """
+
+    @staticmethod
+    def _check_X_y_decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        @wraps(func)
+        def wrapper(X: np.ndarray, y: np.ndarray, *args, **kwargs) -> Any:
+            X, y = check_X_y(X, y)
+            return func(X, y, *args, **kwargs)
+
+        return wrapper
+
+    @staticmethod
+    def _check_array_decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        @wraps(func)
+        def wrapper(X: np.ndarray, *args, **kwargs) -> Any:
+            X = check_array(X)
+            return func(X, *args, **kwargs)
+
+        return wrapper
+
+    @final
+    def fit(self, X: np.ndarray, y: np.ndarray = None) -> "CustomBaseEstimator":
+        if y is None:
+            return self._check_array_decorator(self._fit)(X)
+        return self._check_X_y_decorator(self._fit)(X, y)
+
+    @final
+    def transform(self, X: np.ndarray) -> np.ndarray:
+        return self._check_array_decorator(self._transform)(X)
+
+    def _fit(self, X: np.ndarray, y: np.ndarray = None) -> "CustomBaseEstimator":
+        raise NotImplementedError("Subclasses should implement _fit method")
+
+    def _transform(self, X: np.ndarray) -> np.ndarray:
+        raise NotImplementedError("Subclasses should implement _transform method")
