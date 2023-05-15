16494ddf7d0b51f1e36c9b57d05a561444ed7dbe
Provost Simon <simon1.provost@epitech.eu>
2023-05-15 21:08:25 +0100

    refactor(estimators): add lexicoRF experiment reproduction
    - Add experiment engine
    - Improve class
    - Remove pylint long process from pre-commit

--- scikit_longitudinal/estimators/tree/__init__.py
+++ scikit_longitudinal/estimators/tree/__init__.py
@@ -1,4 +1,4 @@
-from .lexico_rf import LexicoRF  # noqa
-from .nested_tree import NestedTreesClassifier  # noqa
+from scikit_longitudinal.estimators.tree.lexico_rf.lexico_rf import LexicoDecisionTree, LexicoRF  # noqa
+from scikit_longitudinal.estimators.tree.nested_tree.nested_tree import NestedTreesClassifier  # noqa
 
-_all_ = ["NestedTreesClassifier", "LexicoRF"]
+_all_ = ["NestedTreesClassifier", "LexicoRF", "LexicoDecisionTree"]
