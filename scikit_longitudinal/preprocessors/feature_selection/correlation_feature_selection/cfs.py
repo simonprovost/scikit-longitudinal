beae24e71a124ecc1f1d6c31c4c78a27ab700e4a
Provost Simon <simon1.provost@epitech.eu>
2023-09-30 00:16:59 +0100

    refactor(preprocessing): improve correlation-based feature selection algs
    - Split CFS and CFS Per Group
    - Update types and documentations
    - Preprocessing is now Preprocessors

---
+++ scikit_longitudinal/preprocessors/feature_selection/correlation_feature_selection/cfs.py
@@ -0,0 +1,101 @@
+import numpy as np
+from overrides import override
+
+from scikit_longitudinal.preprocessors.feature_selection.correlation_feature_selection.utils import (
+    _exhaustive_search,
+    _greedy_search,
+)
+from scikit_longitudinal.templates import CustomTransformerMixinEstimator
+
+# pylint: disable=R0902
+
+
+class CorrelationBasedFeatureSelection(CustomTransformerMixinEstimator):
+    """Correlation-based Feature Selection (CFS).
+
+    This class performs feature selection using the correlation-based feature selection (CFS) algorithm on given data.
+    The CFS algorithm is a filter method that selects features based on their correlation with the target variable and
+    their mutual correlation with each other. This implementation supports the following search methods:
+    exhaustiveSearch, or greedySearch. This implementation concern the support for CFS only. For the CFS per group
+    (longitudinal component), refer to the CorrelationBasedFeatureSelectionPerGroup class.
+
+    Read more in the Notes below for implementation details.
+
+    Args:
+        search_method : str, default="greedySearch"
+            The search method to use. Options are "exhaustiveSearch", and "greedySearch".
+
+    Attributes:
+        selected_features_ : ndarray of shape (n_features,)
+            The indices of the selected features.
+
+    Examples:
+        >>>  # Without the longitudinal component (original CFS):
+        >>> import numpy as np
+        >>> from sklearn.datasets import make_classification
+        >>> from scikit_longitudinal import CorrelationBasedFeatureSelection
+        >>> X, y = make_classification(n_samples=100, n_features=20, random_state=42)
+        >>> cfs = CorrelationBasedFeatureSelection()
+        >>> cfs.fit(X, y)
+        >>> X_selected = cfs.transform(X)
+        >>> X_selected.shape
+        >>> # (100, N) ; N is the number of selected features
+
+    Notes:
+        The improved CFS algorithm is based on the following references:
+
+        * Zixiao. S. (2019, August 11). GitHub - ZixiaoShen
+        /Correlation-based-Feature-Selection, available at:
+        https://github.com/ZixiaoShen/Correlation-based-Feature-Selection
+
+    See also:
+        * CustomTransformerMixinEstimator: Base class for all Transformer Mixin estimators in scikit-learn that we
+        customed so that the original scikit-learn "check_x_y" is performed all the time.
+
+    """
+
+    # pylint: disable=too-many-arguments,invalid-name,signature-differs,no-member
+    def __init__(
+        self,
+        search_method: str = "greedySearch",
+    ):
+        assert search_method in {
+            "exhaustiveSearch",
+            "greedySearch",
+        }, "search_method must be: 'exhaustiveSearch', or 'greedySearch'"
+
+        self.search_method = search_method
+        self.selected_features_ = []
+
+    @override
+    def _fit(self, X: np.ndarray, y: np.ndarray) -> "CorrelationBasedFeatureSelection":
+        """Fits the feature selection algorithm to the given data.
+
+        Args:
+            X (np.ndarray):
+                Input data of shape (n_samples, n_features).
+            y (np.ndarray):
+                Target variable of shape (n_samples).
+
+        Returns:
+            CorrelationBasedFeatureSelection: The fitted feature selection algorithm.
+        """
+        match self.search_method:
+            case "exhaustiveSearch":
+                self.selected_features_ = _exhaustive_search(X, y)
+            case "greedySearch":
+                self.selected_features_ = _greedy_search(X, y)
+        return self
+
+    @override
+    def _transform(self, X: np.ndarray) -> np.ndarray:
+        """Transforms the input data to the selected features.
+
+        Args:
+            X (np.ndarray):
+                Input data of shape (n_samples, n_features).
+
+        Returns:
+            np.ndarray: The transformed data of shape (n_samples, n_selected_features).
+        """
+        return X[:, self.selected_features_]
--- scikit_longitudinal/preprocessors/feature_selection/correlation_feature_selection/cfs_per_group.py
+++ scikit_longitudinal/preprocessors/feature_selection/correlation_feature_selection/cfs_per_group.py
@@ -1,6 +1,5 @@
 import re
 from collections import defaultdict
-from itertools import combinations
 from typing import Dict, List, Optional, Tuple
 
 import numpy as np
@@ -8,19 +7,23 @@ import pandas as pd
 import ray
 from overrides import override
 
-from scikit_longitudinal.preprocessing.feature_selection.cfs_per_group.utils import merit_calculation
+from scikit_longitudinal.preprocessors.feature_selection.correlation_feature_selection.utils import (
+    _exhaustive_search,
+    _greedy_search,
+)
 from scikit_longitudinal.templates import CustomTransformerMixinEstimator
 
 
 # pylint: disable=R0902
 class CorrelationBasedFeatureSelectionPerGroup(CustomTransformerMixinEstimator):
-    """Correlation-based Feature Selection (CFS) per group.
+    """Correlation-based Feature Selection (CFS) per group (CFS Per Group)..
 
     This class performs feature selection using the correlation-based feature selection (CFS) algorithm on given data.
     The CFS algorithm is a filter method that selects features based on their correlation with the target variable and
     their mutual correlation with each other. This implementation supports the following search methods:
-    exhaustiveSearch, or greedySearch. This implementation also supports a longitudinal component to handle feature
-    selection for longitudinal data.
+    exhaustiveSearch, or greedySearch. This implementation concern the support for the longitudinal component to
+    handle feature selection for longitudinal data. Hence, the CFS per group. If no longitudinal component is needed,
+    refer to the CorrelationBasedFeatureSelection class.
 
     Read more in the Notes below for implementation details.
 
@@ -41,14 +44,14 @@ class CorrelationBasedFeatureSelectionPerGroup(CustomTransformerMixinEstimator):
             Whether to use parallel processing for the CFS algorithm (especially useful for the exhaustive search method
             with the CFS per group, i.e. longitudinal component).
 
-        cfs_longitudinal_outer_search_method : str, default=None
+        outer_search_method : str, default=None
             The outer (to the final aggregated list of features) search method to use for the CFS per group
             (longitudinal component). If None, it defaults to the same as the `search_method`.
 
-        cfs_longitudinal_inner_search_method : str, default="exhaustiveSearch"
+        inner_search_method : str, default="exhaustiveSearch"
             The inner (to each group) search method to use for the CFS per group (longitudinal component).
 
-        cfs_per_group_version : str, default=2
+        version : str, default=2
             The version of the CFS per group algorithm to use. Options are "1" and "2". Version 2 is the improved with
             an outer search out of the final aggregated list of features of the first phase. Refer to the paper proposed
             below for more details.
@@ -61,17 +64,6 @@ class CorrelationBasedFeatureSelectionPerGroup(CustomTransformerMixinEstimator):
             The indices of the selected features.
 
     Examples:
-        >>>  # Without the longitudinal component (original CFS):
-        >>> import numpy as np
-        >>> from sklearn.datasets import make_classification
-        >>> from scikit_longitudinal import CorrelationBasedFeatureSelectionPerGroup
-        >>> X, y = make_classification(n_samples=100, n_features=20, random_state=42)
-        >>> cfs = CorrelationBasedFeatureSelectionPerGroup()
-        >>> cfs.fit(X, y)
-        >>> X_selected = cfs.transform(X)
-        >>> X_selected.shape
-        >>> # (100, N) ; N is the number of selected features
-
         >>> # With the longitudinal component:
         >>> group_features = [(0, 1, 2), (3, 4, 5), (6, 7, 8, 9)]
         >>> cfs_longitudinal = CorrelationBasedFeatureSelectionPerGroup(
@@ -93,7 +85,7 @@ class CorrelationBasedFeatureSelectionPerGroup(CustomTransformerMixinEstimator):
         >>> X_longitudinal_selected.shape
         >>> # (100, N) ; N is the number of selected features
 
-        >>> # Example of using the apply_selected_features_and_rename method:
+        >>> # Example of using the apply_selected_features_and_rename method (alternative to transform):
         >>> data = np.random.random((100, 20))
         >>> df = pd.DataFrame(data, columns=[f'feature{i}_w1' for i in range(10)] +
             [f'feature{i}_w2' for i in range(10)])
@@ -119,7 +111,9 @@ class CorrelationBasedFeatureSelectionPerGroup(CustomTransformerMixinEstimator):
           Feature selection for the classification of longitudinal human ageing data.
           In 2017 IEEE International Conference on Data Mining Workshops (ICDMW) (pp. 739-746). IEEE.
 
-        * [VERSION-2 of the CFS Per Group] TODO Add the paper reference here
+        * [VERSION-2 of the CFS Per Group] T. Pomsuwan and A. Freitas, “Feature selection for the classification of
+          longitudinal human ageing data,” Master’s thesis, University of Kent, Feb 2018. [Online]. Available:
+          https://kar.kent.ac.uk/66568/
 
         * Pomsuwan, T. (2023, February 24). GitHub - mastervii/CSF_2-phase-variant, avaiable at:
           https://github.com/mastervii/CSF_2-phase-variant
@@ -132,16 +126,15 @@ class CorrelationBasedFeatureSelectionPerGroup(CustomTransformerMixinEstimator):
 
     # pylint: disable=too-many-arguments,invalid-name,signature-differs,no-member
     def __init__(
-            self,
-            non_longitudinal_features: Optional[List[int]] = None,
-            search_method: str = "greedySearch",
-            features_group: Optional[List[List[int]]] = None,
-            parallel: bool = False,
-            cfs_longitudinal_outer_search_method: str = None,
-            cfs_longitudinal_inner_search_method: str = "exhaustiveSearch",
-            cfs_per_group_version=2,
-            num_cpus: int = -1,
-            cfs_type: str = "cfs_longitudinal",
+        self,
+        non_longitudinal_features: Optional[List[int]] = None,
+        search_method: str = "greedySearch",
+        features_group: Optional[List[List[int]]] = None,
+        parallel: bool = False,
+        outer_search_method: str = None,
+        inner_search_method: str = "exhaustiveSearch",
+        version=1,
+        num_cpus: int = -1,
     ):
         assert search_method in {
             "exhaustiveSearch",
@@ -151,24 +144,13 @@ class CorrelationBasedFeatureSelectionPerGroup(CustomTransformerMixinEstimator):
         self.search_method = search_method
         self.features_group = features_group
         self.parallel = parallel
-        self.cfs_longitudinal_outer_search_method = (
-            self.search_method if cfs_longitudinal_outer_search_method is None else cfs_longitudinal_outer_search_method
-        )
-        self.cfs_longitudinal_inner_search_method = cfs_longitudinal_inner_search_method
-        self.selected_features_ = []
-        self.selected_longitudinal_features_ = []
+        self.outer_search_method = self.search_method if outer_search_method is None else outer_search_method
+        self.inner_search_method = inner_search_method
         self.non_longitudinal_features = non_longitudinal_features
         self.num_cpus = num_cpus
-        if cfs_type == "auto":
-            if self.features_group is not None:
-                self.cfs_type = "cfs_longitudinal"
-            else:
-                self.cfs_type = "cfs"
-        elif cfs_type in {"cfs", "cfs_longitudinal"}:
-            self.cfs_type = cfs_type
-        else:
-            self.cfs_type = "cfs"
-        self.cfs_per_group_version = cfs_per_group_version
+        self.version = version
+        self.selected_features_ = []
+        self.selected_longitudinal_features_ = []
 
     @override
     def _fit(self, X: np.ndarray, y: np.ndarray) -> "CorrelationBasedFeatureSelectionPerGroup":
@@ -188,28 +170,20 @@ class CorrelationBasedFeatureSelectionPerGroup(CustomTransformerMixinEstimator):
             CorrelationBasedFeatureSelectionPerGroup: The fitted instance of the CFS algorithm.
 
         """
-
-        if self.features_group is not None:
-            self.cfs_type = "cfs_longitudinal"
-
-        if self.features_group is not None \
-                and ray.is_initialized() is False \
-                and self.parallel is True \
-                and self.cfs_type != "cfs":
+        if self.features_group is not None and ray.is_initialized() is False and self.parallel is True:
             if self.num_cpus != -1:
                 ray.init(num_cpus=self.num_cpus)
             else:
                 ray.init()
 
-        if self.features_group is not None and self.cfs_type == "cfs_longitudinal":
-            self.search_method = self.cfs_longitudinal_inner_search_method
+        if self.features_group is not None:
+            self.search_method = self.inner_search_method
             group_features_copy, group_selected_features = (
                 (self.features_group.copy(), []) if self.features_group else ([], [])
             )
+
             self.features_group = None
 
-            # Run the inner search method for each group of features in
-            # parallel or not
             if self.parallel:
                 futures = [self._ray_fit_subset.remote(self, X, y, group) for group in group_features_copy]
                 while futures:
@@ -220,30 +194,25 @@ class CorrelationBasedFeatureSelectionPerGroup(CustomTransformerMixinEstimator):
             else:
                 group_selected_features = [self._fit_subset(X, y, group) for group in group_features_copy]
 
-            if self.cfs_per_group_version == 2:
-                # Combine inner search results with non-longitudinal features
+            if self.version == 2:
                 combined_features = [
-                                        index for sublist in group_selected_features for index in sublist
-                                    ] + self.non_longitudinal_features
-
-                # Run the outer search method on the final set of features
-                # extracted from each group
-                self.search_method = self.cfs_longitudinal_outer_search_method
+                    index for sublist in group_selected_features for index in sublist
+                ] + self.non_longitudinal_features
+                self.search_method = self.outer_search_method
                 selected_indices = self._fit(X[:, combined_features], y).selected_features_
-
-                # Get the final set of selected features based on the outer search results
                 flattened_list = np.array(combined_features)
                 self.selected_features_ = flattened_list[selected_indices].tolist()
-            elif self.cfs_per_group_version == 1:
-                # Only use the inner search results as the selected features for version 1
+            elif self.version == 1:
                 flattened_list = np.array([index for sublist in group_selected_features for index in sublist])
                 self.selected_features_ = (flattened_list.tolist() or []) + (self.non_longitudinal_features or [])
+            else:
+                raise ValueError(f"Version {self.version} is not supported. Please choose version 1 or 2.")
         else:
             match self.search_method:
                 case "exhaustiveSearch":
-                    self.selected_features_ = self._exhaustive_search(X, y)
+                    self.selected_features_ = _exhaustive_search(X, y)
                 case "greedySearch":
-                    self.selected_features_ = self._greedy_search(X, y)
+                    self.selected_features_ = _greedy_search(X, y)
 
         return self
 
@@ -259,77 +228,7 @@ class CorrelationBasedFeatureSelectionPerGroup(CustomTransformerMixinEstimator):
             The reduced input data as a numpy array of shape (n_samples, n_selected_features).
 
         """
-        if self.cfs_type == "cfs_longitudinal":
-            return X
-        return X[:, self.selected_features_]
-
-    def _greedy_search(self, X: np.ndarray, y: np.ndarray) -> List[int]:
-        """Performs greedy search for feature selection.
-
-         This method starts with an empty set of features and iteratively adds
-         or removes a feature based on the merit score, which takes into account
-         the correlation with the target variable and the inter-feature correlations.
-         The process continues until no further improvement in the merit score is achieved.
-
-        Args:
-            X (np.ndarray):
-                Input data of shape (n_samples, n_features).
-            y (np.ndarray):
-                Target variable of shape (n_samples).
-
-        Returns:
-            List[int]: A list of selected feature indices.
-
-        """
-        n_features = X.shape[1]
-        selected_features: List[int] = []
-
-        while True:
-            current_merit = merit_calculation(X[:, selected_features], y)
-
-            add_candidates = [
-                (merit_calculation(X[:, selected_features + [i]], y), i)
-                for i in range(n_features)
-                if i not in selected_features
-            ]
-            remove_candidates = [
-                (merit_calculation(X[:, [i for i in selected_features if i != j]], y), j) for j in selected_features
-            ]
-
-            all_candidates = add_candidates + remove_candidates
-            best_merit, best_idx = max(all_candidates, key=lambda x: x[0], default=(-np.inf, -1))
-
-            if best_merit <= current_merit:
-                break
-
-            if best_idx in selected_features:
-                selected_features.remove(best_idx)
-            else:
-                selected_features.append(best_idx)
-        return selected_features
-
-    def _exhaustive_search(self, X: np.ndarray, y: np.ndarray) -> List[int]:
-        """Performs exhaustive search for feature selection.
-
-        This method examines all possible combinations of features and selects
-        the combination that has the highest merit score.
-
-        Args:
-            X (np.ndarray):
-                Input data of shape (n_samples, n_features).
-            y (np.ndarray):
-                Target variable of shape (n_samples).
-
-        Returns:
-            List[int]: A list of selected feature indices.
-
-        """
-        n_features = X.shape[1]
-        return max(
-            (feature_set for r in range(1, n_features + 1) for feature_set in combinations(range(n_features), r)),
-            key=lambda feature_set: merit_calculation(X[:, feature_set], y),
-            default=[],
-        )
+        return X
 
     def _fit_subset(self, X: np.ndarray, y: np.ndarray, group: Tuple[int]) -> List[int]:
         """Fits the CFS algorithm on a subset of the input data specified by the group.
@@ -378,7 +277,7 @@ class CorrelationBasedFeatureSelectionPerGroup(CustomTransformerMixinEstimator):
     # pylint: disable=W9016
     @staticmethod
     def apply_selected_features_and_rename(
-            df: pd.DataFrame, selected_features: List, regex_match=r"^(.+)_w(\d+)$"
+        df: pd.DataFrame, selected_features: List, regex_match=r"^(.+)_w(\d+)$"
     ) -> [pd.DataFrame, None]:
         """Apply selected features to the input DataFrame and rename non-longitudinal features.
 
