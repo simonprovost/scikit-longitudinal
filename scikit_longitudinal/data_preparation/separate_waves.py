9bea58d3ac61368265721c63efbb235473b4a8b4
Provost Simon <simon1.provost@epitech.eu>
2023-09-30 00:11:20 +0100

    refactor(data_preparation): update SepWav template/AutoLD's compliance
    - Plus improved documentation

--- scikit_longitudinal/data_preparation/separate_waves.py
+++ scikit_longitudinal/data_preparation/separate_waves.py
@@ -8,6 +8,7 @@ from sklearn.base import BaseEstimator, ClassifierMixin, clone
 from sklearn.ensemble import StackingClassifier, VotingClassifier
 from sklearn.exceptions import NotFittedError
 
+from scikit_longitudinal.data_preparation.longitudinal_dataset import clean_padding
 from scikit_longitudinal.templates.custom_data_preparation_mixin import DataPreparationMixin
 
 
@@ -15,14 +16,15 @@ def validate_extract_wave_input(func):
     """Decorator to validate the input to the _extract_wave function.
 
     Args:
-        func (function): Function to be decorated.
+        func (function):
+            Function to be decorated.
 
     Returns:
-        function: Decorated function.
+        function:
+            Decorated function.
 
     Raises:
         ValueError: If the wave number is less than 0.
-
     """
 
     def wrapper(self, wave: int):
@@ -37,14 +39,15 @@ def validate_extract_wave_output(func):  # pragma: no cover
     """Decorator to validate the output of the _extract_wave function.
 
     Args:
-        func (function): Function to be decorated.
+        func (function):
+            Function to be decorated.
 
     Returns:
-        function: Decorated function.
+        function:
+            Decorated function.
 
     Raises:
         ValueError: If the number of features in the wave does not match the expected number of features.
-
     """
 
     def wrapper(self, wave: int):
@@ -63,14 +66,14 @@ def validate_fit_input(func):
     """Decorator to validate the input to the fit function.
 
     Args:
-        func (function): Function to be decorated.
+        func (function):
+            Function to be decorated.
 
     Returns:
         function: Decorated function.
 
     Raises:
         ValueError: If the classifier, dataset, or feature groups are None.
-
     """
 
     def wrapper(self, X, y):
@@ -88,11 +91,11 @@ def validate_fit_output(func):  # pragma: no cover
         func (function): Function to be decorated.
 
     Returns:
-        function: Decorated function.
+        function:
+            Decorated function.
 
     Raises:
         ValueError: If the clf_ensemble is None after fitting.
-
     """
 
     def wrapper(self, X, y):
@@ -108,14 +111,15 @@ def validate_predict_input(func):
     """Decorator to validate the input to the predict function.
 
     Args:
-        func (function): Function to be decorated.
+        func (function):
+            Function to be decorated.
 
     Returns:
-        function: Decorated function.
+        function:
+            Decorated function.
 
     Raises:
         NotFittedError: If the SepWav instance is not fitted yet.
-
     """
 
     def wrapper(self, X):
@@ -133,14 +137,15 @@ def validate_predict_wave_input(func):
     """Decorator to validate the input to the predict wave function.
 
     Args:
-        func (function): Function to be decorated.
+        func (function):
+            Function to be decorated.
 
     Returns:
-        function: Decorated function.
+        function:
+            Decorated function.
 
     Raises:
         NotFittedError: If the SepWav instance is not fitted yet.
-
     """
 
     def wrapper(self, wave, X):
@@ -158,6 +163,26 @@ def validate_predict_wave_input(func):
 
 @ray.remote
 def train_classifier(classifier, X_wave, y_wave, wave):  # pragma: no cover
+    """Train a classifier on a specific wave.
+
+    This function is used for parallel processing by leveraging Ray framework.
+
+    Args:
+        classifier (BaseEstimator):
+            The classifier to employ.
+        X_wave (DataFrame):
+            The input samples for the wave. Each row represents an observation,
+            and each column represents a feature.
+        y_wave (Series):
+            The target values for the wave.
+        wave (int):
+            The wave number to use as for the returned classifier by denoting it as wave_{wave number}.
+
+    Returns:
+        tuple: A tuple containing two elements:
+            - wave (string): The wave number as a string.
+            - clf_wave (BaseEstimator): The trained classifier.
+    """
     clf_wave = clone(classifier)
     clf_wave.fit(X_wave, y_wave)
     return f"wave_{wave}", clf_wave
@@ -167,30 +192,46 @@ def train_classifier(classifier, X_wave, y_wave, wave):  # pragma: no cover
 class SepWav(BaseEstimator, ClassifierMixin, DataPreparationMixin):
     """The SepWav (Separate Waves) class for data transformation in longitudinal data analysis.
 
-    This technique involves supplying an algorithm defined by the user one wave at a time. Predicting a class
-    variable, the algorithm is trained on a dataset containing only the current wave (beginning with the first). For
-    each successive wave, the algorithm is trained using a newly-created dataset to which the new wave is added. This
-    produces a number of classifiers. Then, these classifiers can be combined into an ensemble model or utilised in
-    other ways based on user-defined methods. If Predict is invoked, the ensemble model will be used to predict the
-    class variable. Nevertheless, if the user wishes to predict the class variable for a particular wave,
-    they can invoke the predict_wave method. Or last way, merely by getting the list of classifiers into their script
-    and playing with them. Ensemble strategies supported are voting and stacking.
+    The proposed methodology entails the provision of a user-defined algorithm in a sequential manner, wave by wave.
+    The algorithm is designed to predict a class variable by being trained on a dataset that exclusively consists of
+    the current wave, starting from the first wave. The algorithm undergoes training for each subsequent wave by
+    utilising a freshly generated dataset that incorporates the newly introduced wave. The execution of this code
+    snippet results in the generation of multiple classifiers. Next, it is possible to merge these classifiers into
+    an ensemble model or employ them in alternative manners according to methods defined by the user. When the
+    Predict function is called, the ensemble model will be utilised to make predictions on the class variable. To
+    facilitate the prediction of the class variable for a specific wave, the user can utilise the predict_wave
+    method. Alternatively, users can incorporate the list of classifiers into their script and manipulate them
+    accordingly. The supported ensemble strategies encompass voting and stacking methodologies.
 
     Attributes:
-        dataset (LongitudinalDataset): The dataset to be used.
-        classifier (BaseEstimator): The classifier to be used.
-        classifiers (List): A list of classifiers for each wave.
-        ensemble_strategy (str): The strategy for combining the classifiers ('voting' or 'stacking').
-        clf_ensemble (BaseEstimator): The combined classifier.
-        voting_weights (List[float]): The weights for the voting classifier.
-        voting_strategy (str): The voting strategy for the voting classifier ('hard' or 'soft').
-        n_jobs (int): The number of jobs to run in parallel.
-        stacking_final_estimator (BaseEstimator): The final estimator to be used in the stacking classifier.
-        stacking_cv (int): The cross-validation strategy for the stacking classifier.
-        stacking_stack_method (str): The method to be used for the stacking classifier.
-        stacking_passthrough (bool): Whether to pass the inputs to the final estimator for the stacking classifier.
-        parallel (bool): Whether to run the fit waves in parallel.
-        num_cpus (int): The number of cpus to use for parallel processing.
+        dataset (LongitudinalDataset):
+            The dataset to be used.
+        classifier (BaseEstimator):
+            The classifier to be used.
+        classifiers (List):
+            A list of classifiers for each wave.
+        ensemble_strategy (str):
+            The strategy for combining the classifiers ('voting' or 'stacking').
+        clf_ensemble (BaseEstimator):
+            The combined classifier.
+        voting_weights (List[float]):
+            The weights for the voting classifier.
+        voting_strategy (str):
+            The voting strategy for the voting classifier ('hard' or 'soft').
+        n_jobs (int):
+            The number of jobs to run in parallel.
+        stacking_final_estimator (BaseEstimator):
+            The final estimator to be used in the stacking classifier.
+        stacking_cv (int):
+            The cross-validation strategy for the stacking classifier.
+        stacking_stack_method (str):
+            The method to be used for the stacking classifier.
+        stacking_passthrough (bool):
+            Whether to pass the inputs to the final estimator for the stacking classifier.
+        parallel (bool):
+            Whether to run the fit waves in parallel.
+        num_cpus (int):
+            The number of cpus to use for parallel processing.
 
     Example:
         ```python
@@ -216,12 +257,11 @@ class SepWav(BaseEstimator, ClassifierMixin, DataPreparationMixin):
         y_pred = sepwav.predict(longitudinal_data.X_test)
         print(classification_report(longitudinal_data.y_test, y_pred))
         ```
-
     """
 
     def __init__(
         self,
-        features_group: List[List[Union[int, str]]] = None,
+        features_group: List[List[int]] = None,
         classifier: ["BaseEstimator"] = None,
         non_longitudinal_features: List[Union[int, str]] = None,
         feature_list_names: List[str] = None,
@@ -286,7 +326,8 @@ class SepWav(BaseEstimator, ClassifierMixin, DataPreparationMixin):
         and slices the dataset to create the training data for the wave.
 
         Args:
-            wave (int): The wave number to extract. This should be a non-negative integer.
+            wave (int):
+                The wave number to extract. This should be a non-negative integer.
 
         Returns:
             tuple: A tuple containing two elements:
@@ -296,7 +337,6 @@ class SepWav(BaseEstimator, ClassifierMixin, DataPreparationMixin):
 
         Raises:
             ValueError: If the wave number is less than 0.
-
         """
         feature_indices = [group[wave] for group in self.features_group if wave < len(group)]
         if self.non_longitudinal_features is not None:
@@ -313,15 +353,19 @@ class SepWav(BaseEstimator, ClassifierMixin, DataPreparationMixin):
     def fit(self, X: Union[List[List[float]], "np.ndarray"], y: Union[List[float], "np.ndarray"]):
         """Fits the model to the given data.
 
-        This method goes through each wave in the dataset, extracts the corresponding data, and trains a clone
-        of the classifier on it. It then adds the trained classifier to a list.
+        The following method iterates over each wave present in the dataset, retrieves the relevant data associated
+        with each wave, and proceeds to train a replica of the classifier using said data. The trained classifier is
+        subsequently appended to a pre-existing list.
 
-        After all waves have been processed, it creates an ensemble classifier using the specified ensemble strategy,
-        and fits it on the training data.
+        Once the processing of all waves is complete, the system proceeds to generate an ensemble classifier
+        utilising the designated ensemble strategy. Subsequently, the ensemble classifier is trained on the provided
+        training data for the final prediction of the class variable of new observations.
 
         Args:
-            X (Union[List[List[float]], np.ndarray]): The input samples.
-            y (Union[List[float], np.ndarray]): The target values.
+            X (Union[List[List[float]], np.ndarray]):
+                The input samples.
+            y (Union[List[float], np.ndarray]):
+                The target values.
 
         Returns:
             self: Returns self.
@@ -329,10 +373,12 @@ class SepWav(BaseEstimator, ClassifierMixin, DataPreparationMixin):
         Raises:
             ValueError: If the classifier, dataset, or feature groups are None, or if the ensemble strategy
               is neither 'voting' nor 'stacking'.
-
         """
         self.dataset = pd.DataFrame(X, columns=self.feature_list_names)
         self.target = y
+
+        if self.features_group is not None:
+            self.features_group = clean_padding(self.features_group)
 
         if self.parallel:
             futures = [
@@ -352,7 +398,10 @@ class SepWav(BaseEstimator, ClassifierMixin, DataPreparationMixin):
 
         if self.ensemble_strategy == "voting":
             self.clf_ensemble = VotingClassifier(
-                self.classifiers, voting=self.voting_strategy, weights=self.voting_weights, n_jobs=self.n_jobs,
+                self.classifiers,
+                voting=self.voting_strategy,
+                weights=self.voting_weights,
+                n_jobs=self.n_jobs,
             )
         elif self.ensemble_strategy == "stacking":
             self.clf_ensemble = StackingClassifier(self.classifiers, final_estimator=self.classifier)
@@ -361,9 +410,8 @@ class SepWav(BaseEstimator, ClassifierMixin, DataPreparationMixin):
 
         X_data = self.dataset.values
 
-        if hasattr(X_data, "flags"):
-            if not X_data.flags['C_CONTIGUOUS']:
-                X_data = np.ascontiguousarray(X_data)
+        if hasattr(X_data, "flags") and not X_data.flags["C_CONTIGUOUS"]:
+            X_data = np.ascontiguousarray(X_data)
 
         self.clf_ensemble.fit(X_data, self.target)
 
@@ -373,16 +421,19 @@ class SepWav(BaseEstimator, ClassifierMixin, DataPreparationMixin):
     def predict(self, X: Union[List[List[float]], "np.ndarray"]) -> Union[List[float], "np.ndarray"]:
         """Predict class for X.
 
-        The predicted class of an input sample is computed as the class with the highest mean predicted probability.
-        If ‘voting=’hard’, the predicted classes of the underlying classifiers are used for majority rule voting. If
-        ‘voting=’soft’, sums predicted probabilities of the underlying classifiers are used to predict the class label.
+        The computation of the predicted class for an input sample involves determining the class that possesses the
+        highest mean predicted probability. When the parameter 'voting' is set to 'hard', the predicted classes
+        obtained from the underlying classifiers are utilised for the purpose of majority rule voting. When the value
+        of the variable 'voting' is set to 'soft', the predicted probabilities of the underlying classifiers are
+        summed together in order to determine the predicted class label.
 
         Args:
-            X (Union[List[List[float]], np.ndarray)): The input samples.
+            X (Union[List[List[float]], np.ndarray)):
+                The input samples.
 
         Returns:
-            Union[List[float], np.ndarray]: The predicted classes.
-
+            Union[List[float], np.ndarray]:
+                The predicted classes.
         """
         return self.clf_ensemble.predict(X)
 
@@ -390,30 +441,38 @@ class SepWav(BaseEstimator, ClassifierMixin, DataPreparationMixin):
     def predict_proba(self, X: Union[List[List[float]], "np.ndarray"]) -> Union[List[List[float]], "np.ndarray"]:
         """Predict class probabilities for X.
 
-        The predicted class probabilities of an input sample is computed as the probabilities predicted by the underlying classifiers.
+        The predicted class probabilities of an input sample is computed as the probabilities predicted by
+        the underlying classifiers.
 
         Args:
-            X (Union[List[List[float]], np.ndarray)): The input samples.
+            X (Union[List[List[float]], np.ndarray)):
+                The input samples.
 
         Returns:
-            Union[List[List[float]], np.ndarray]: The predicted class probabilities.
-
+            Union[List[List[float]], np.ndarray]:
+                The predicted class probabilities.
         """
-        if hasattr(self.clf_ensemble, 'predict_proba'):
+        if hasattr(self.clf_ensemble, "predict_proba"):
             return self.clf_ensemble.predict_proba(X)
         else:
-            raise NotImplementedError(f"predict_proba is not implemented for this classifier: {self.clf_ensemble} / type: {type(self.clf_ensemble)}")
+            raise NotImplementedError(
+                f"predict_proba is not implemented for this classifier: "
+                f"{self.clf_ensemble} / type: {type(self.clf_ensemble)}"
+            )
 
     @validate_predict_wave_input
     def predict_wave(self, wave: int, X: Union[List[List[float]], "np.ndarray"]) -> Union[List[float], "np.ndarray"]:
-        """Predict class for X, using the number of the wave.
+        """Predict class for X, using the classifier for the specified wave number.
 
         Args:
-            wave (int): The wave number to extract.
-            X (Union[List[List[float]], np.ndarray)): The input samples.
+            wave (int):
+                The wave number to extract.
+            X (Union[List[List[float]], np.ndarray)):
+                The input samples.
 
         Returns:
-            Union[List[float], np.ndarray]: The predicted classes.
+            Union[List[float], np.ndarray]:
+                The predicted classes.
 
         """
         return self.classifiers[wave][1].predict(X)
