b829484d4b12257d408ce2380f5131513d3c6c3c
Provost Simon <simon1.provost@epitech.eu>
2025-12-01 18:52:02 +0000

    release-pypi: 0.1.6->0.1.7 — Support py313 [cd build]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup  # pragma: no cover
 
 setup(  # pragma: no cover
     name="Scikit-longitudinal",
-    version="0.1.6",
+    version="0.1.7",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/simonprovost/scikit-longitudinal",
